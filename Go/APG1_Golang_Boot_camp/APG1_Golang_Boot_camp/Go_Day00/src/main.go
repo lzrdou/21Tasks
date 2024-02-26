5c9b4cd1ac25dcf1e8b3b18684b87f677e0924f1
artemiy <butyrin.artemiy@krasnoe-beloe.ru>
2024-02-26 13:42:52 +0300

    mean, median, mode done

--- Go/APG1_Golang_Boot_camp/APG1_Golang_Boot_camp/Go_Day00/src/main.go
+++ Go/APG1_Golang_Boot_camp/APG1_Golang_Boot_camp/Go_Day00/src/main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	var nums []int
-	var num int
+	var num, mode int
+	var median float32
+
 	for {
 		_, err := fmt.Scan(&num)
 		if err == io.EOF {
@@ -20,6 +22,32 @@ func main() {
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] < nums[j]
 	})
-	fmt.Println(nums)
+
+	l := len(nums)
+
+	cnt := make(map[int]int)
+	sum := 0
+	for _, num := range nums {
+		cnt[num] += 1
+		sum += num
+	}
+	fmt.Printf("Mean: %f\n", float32(sum)/float32(l))
+
+	if l%2 == 1 {
+		median = float32(nums[l/2])
+	} else {
+		median = (float32(nums[l/2]) + float32(nums[l/2+1])) / 2
+	}
+
+	maxFreq := 0
+	for _, key := range nums {
+		freq := cnt[key]
+		if freq > maxFreq {
+			mode = key
+			maxFreq = freq
+		}
+	}
+	fmt.Printf("Median: %f\n", median)
+	fmt.Printf("Mode %d\n", mode)
 
 }
